7b7f1c1ed717875837a11462a099d93f7ae4d735
seid ahmetovic <107319842+seyyy0@users.noreply.github.com>
2022-12-01 18:05:49 +0100

    Add files via upload

--- PokeApi/app.js
+++ PokeApi/app.js
@@ -2,6 +2,9 @@ const promises = [];
 var i = 0;
 const rightButton = document.getElementById("buttonRight");
 const leftButton = document.getElementById("buttonLeft");
+const pName = document.getElementById("pokemonName");
+const pType = document.getElementById("pokemonType");
+const CurrentImg = document.getElementById("PokemonSprite");
 const fetchPokeApi = () => {
   // Getting data of first 150 pokemon into an array of objects
   for (let i = 1; i <= 150; i++) {
@@ -15,12 +18,12 @@ const fetchPokeApi = () => {
       sprite: data.sprites.other.dream_world["front_default"],
       type: data.types.map((type) => type.type.name).join(" & "),
     }));
-    rightButton.addEventListener("click", nextPokemon);
+    rightButton.addEventListener("click", nextPokemon); // Going right
     function nextPokemon() {
       i++;
       CurrentPokemon(Pokemon[i]);
     }
-    leftButton.addEventListener("click", previousPokemon);
+    leftButton.addEventListener("click", previousPokemon); // Going left
     function previousPokemon() {
       i--;
       CurrentPokemon(Pokemon[i]);
@@ -30,7 +33,9 @@ const fetchPokeApi = () => {
 };
 fetchPokeApi();
 
-const CurrentImg = document.getElementById("PokemonSprite"); // Setting of first/next/prev pokemon sprite
 const CurrentPokemon = (Pokemon) => {
+  // Setting of data of current pokemon
   CurrentImg.src = Pokemon.sprite;
+  pName.innerText = Pokemon.name;
+  pType.innerText = Pokemon.type;
 };
